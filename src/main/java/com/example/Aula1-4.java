11e77281a3d3eb4ff46143d6bc8a48ac5a7a0d05
Giuliano Fujimoto <giuliano.suetugo@gmail.com>
2023-07-29 14:07:35 +0000

    Remover Aula1-4

--- src/main/java/com/example/Aula1-4.java
+++
@@ -1,9 +0,0 @@
-package com.example;
-
-public class Aula1-4 {
-    var idade = 42;
-    double peso = 70;
-    char sexo = 'M';
-
-    System.out.println("Idade é " + idade + "Peso é " + peso + "Sexo é " + sexo);
-}
