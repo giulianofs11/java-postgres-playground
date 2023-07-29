8110702c71c580bc5fb8e20d4dc2db5c5c84dd4b
Giuliano Fujimoto <giuliano.suetugo@gmail.com>
2023-07-29 13:59:57 +0000

    Adicionado Aula1-4

---
+++ src/main/java/com/example/Aula1-4.java
@@ -0,0 +1,9 @@
+package com.example;
+
+public class Aula1-4 {
+    var idade = 42;
+    double peso = 70;
+    char sexo = 'M';
+
+    System.out.println("Idade é " + idade + "Peso é " + peso + "Sexo é " + sexo);
+}
