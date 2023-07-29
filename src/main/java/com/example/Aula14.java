865ddffa672562ffa41fad7ec519fef0fdb61528
Giuliano Fujimoto <giuliano.suetugo@gmail.com>
2023-07-29 14:24:21 +0000

    Atualizado Aula14

--- src/main/java/com/example/Aula14.java
+++ src/main/java/com/example/Aula14.java
@@ -2,7 +2,19 @@ package com.example;
 
 public class Aula14 {
     public static void main(String[] args) {
-        System.out.println("Oi");
+        int idadeInt = 35;
+        byte idade = (byte) idadeInt; 
+        
+        double peso = 80.5;
+        char sexo = 'M';
+        boolean necessidadesEspeciais = true;
+
+// Com var você define qual tipo.
+        var anonascimento = 1970;
+
+        System.out.println("A idade é " + idade + " , o peso é " + peso + " e o sexo é " + sexo);
+        System.out.println("Necessidades especiais: " + necessidadesEspeciais);
+        System.out.println("Ano nascimento: " + anonascimento);
         
     }
 }
